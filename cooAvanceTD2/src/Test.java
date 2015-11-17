6c422b02e0ae4a00624bfeccd69407673bbdb8ce
alanJaouen <alan.jaouen.96@gmail.com>
2015-11-17 14:17:37 +0100

    fez

--- cooAvanceTD2/src/Test.java
+++ cooAvanceTD2/src/Test.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class test {
+public class Test {
 
 	public static void main(String[] args) {
 		Casier casier=new Casier(20);
@@ -8,12 +8,13 @@ public class test {
 		Robot bob=new AutoTracteur(casier);
 		
 		Scanner sc= new Scanner(System.in);
-		System.out.println("1:bouger\n"
-				+ "2:prendre\n"
-				+ "3:lacher");
+		System.out.println("1: bouger\n"
+				+ "2: prendre\n"
+				+ "3: lacher\n"
+				+ "4: quitter");
 		while (true)
 		{
-			System.out.println(bob.monCasier.affiche());
+			System.out.println(casier.affiche());
 			switch(sc.nextInt())
 			{
 			case 1:
@@ -24,6 +25,10 @@ public class test {
 				break;
 			case 3:
 				bob.manipulerRelacher();
+				break;
+			case 4:
+				sc.close();
+				System.exit(0);
 			}
 			
 		}
